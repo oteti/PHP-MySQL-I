9db021a9215b0d6aae8999f68a190877696a12a2
Oteti <otete14@gmail.com>
2018-01-15 02:50:25 -0200

    preciso dormir, amanha termino

---
+++ praticando.php
@@ -0,0 +1,10 @@
+<?php
+$arrayAleatorio = array(1,4,6,3,7,7,3,5,2 );
+function RecebeNumeros($arrayAleatorio)
+{
+  for ($i=0; $i < sizeof($arrayAleatorio); $i++) {
+    $somaDosNumeros =  $arrayAleatorio[i]
+  }
+  return
+}
+?>
